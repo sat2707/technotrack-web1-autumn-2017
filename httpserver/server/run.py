983df273e4443cc52a1f560ab0e52e92fa348719
Stycenko Ilia <stitsenco@gmail.com>
2017-09-22 20:49:24 +0300

    gitignore, httpserver

---
+++ httpserver/server/run.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import socket
+
+
+def get_response(request):
+
+    return 'WRITE YOU RESPONSE HERE'
+
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind(('localhost', 8000))  #
+server_socket.listen(0)  #
+
+print 'Started'
+
+while 1:
+    try:
+        (client_socket, address) = server_socket.accept()
+        print 'Got new client'
+        request_string = client_socket.recv(2048)  #
+        client_socket.send(get_response(request_string))  #
+        client_socket.close()
+    except KeyboardInterrupt:  #
+        print 'Stopped'
+        server_socket.close()  #
+        exit()
